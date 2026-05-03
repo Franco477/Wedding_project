8392c9dadcb53d67ff8af6440fba75e285118d43
Filipao <andresfranco1967.1967@gmail.com>
2026-05-03 16:31:41 -0500

    Fixing port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, abort
 import json
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +17,5 @@ def invite(guest_id):
     return render_template("invite.html", name=guest["name"])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
